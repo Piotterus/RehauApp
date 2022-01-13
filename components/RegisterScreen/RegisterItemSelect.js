a8b23f6ca1732bd37ea96140ad5c5f3ca711a266
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2022-01-13 13:09:51 +0100

    picker dla iosa

--- components/RegisterScreen/RegisterItemSelect.js
+++ components/RegisterScreen/RegisterItemSelect.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {Image, StyleSheet, View, TouchableOpacity, Dimensions, Text, TextInput} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import {Image, StyleSheet, View, TouchableOpacity, Dimensions, Text, TextInput, Platform} from 'react-native';
+import {Picker, PickerIOS} from '@react-native-picker/picker';
 
 export default class RegisterItemSelect extends React.Component {
 
@@ -40,21 +40,40 @@ export default class RegisterItemSelect extends React.Component {
         }
         return (
             <View style={styles.textInputView}>
+                {Platform.OS === 'android' &&
                 <Picker
                     style={styles.textInput}
                     selectedValue={this.props.value}
                     onValueChange={(itemValue, itemIndex) =>
                         this.props.updateValue(itemValue, fieldName)
                     }>
-                    <Picker.Item label="wybierz" value="" />
-                    <Picker.Item label="Artur Jakonis" value="900" />
-                    <Picker.Item label="Ireneusz Mojsiewicz" value="901" />
-                    <Picker.Item label="Krzysztof Twardy" value="902" />
-                    <Picker.Item label="Mariusz Kuleta" value="903" />
-                    <Picker.Item label="Michał Kot" value="904" />
-                    <Picker.Item label="Paweł Kowal" value="905" />
-                    <Picker.Item label="Piotr Bawolski" value="906" />
+                    <Picker.Item label="wybierz" value=""/>
+                    <Picker.Item label="Artur Jakonis" value="900"/>
+                    <Picker.Item label="Ireneusz Mojsiewicz" value="901"/>
+                    <Picker.Item label="Krzysztof Twardy" value="902"/>
+                    <Picker.Item label="Mariusz Kuleta" value="903"/>
+                    <Picker.Item label="Michał Kot" value="904"/>
+                    <Picker.Item label="Paweł Kowal" value="905"/>
+                    <Picker.Item label="Piotr Bawolski" value="906"/>
                 </Picker>
+                }
+                {Platform.OS === 'ios' &&
+                <PickerIOS
+                    style={styles.textInput}
+                    selectedValue={this.props.value}
+                    onValueChange={(itemValue, itemIndex) =>
+                        this.props.updateValue(itemValue, fieldName)
+                    }>
+                    <PickerIOS.Item label="wybierz" value=""/>
+                    <PickerIOS.Item label="Artur Jakonis" value="900"/>
+                    <PickerIOS.Item label="Ireneusz Mojsiewicz" value="901"/>
+                    <PickerIOS.Item label="Krzysztof Twardy" value="902"/>
+                    <PickerIOS.Item label="Mariusz Kuleta" value="903"/>
+                    <PickerIOS.Item label="Michał Kot" value="904"/>
+                    <PickerIOS.Item label="Paweł Kowal" value="905"/>
+                    <PickerIOS.Item label="Piotr Bawolski" value="906"/>
+                </PickerIOS>
+                }
                 <Text style={styles.placeHolderText}>{this.props.text}</Text>
             </View>
         )
